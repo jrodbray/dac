1ca64feb2dcdeab2f135a26ef06da17f7290135a
Rod Bray <rod.bray@scottambler.com>
2015-02-25 11:58:17 -0500

    Certificate emails & autocomplete

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -36,6 +36,12 @@
  */
 	CakePlugin::routes();
 
+
+/**
+ * Set up router to parse extensions like json
+ */
+    Router::parseExtensions();
+
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
